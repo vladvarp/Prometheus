ba437935b35927ab3d6aa42bd2bafc1ea2239c38
vladvarp <47262595+vladvarp@users.noreply.github.com>
2026-03-02 02:44:02 +0300

    1

--- WhiteList/clone.bat
+++ WhiteList/clone.bat
@@ -1,3 +1,4 @@
 curl.exe -O https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt
 curl.exe -O https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt
 curl.exe -O https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt
+curl.exe -O https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt
